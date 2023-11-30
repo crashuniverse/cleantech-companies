9eb0afd1223cb9e4d64a4613fb3c64da09845802
Priya Ranjan Singh <mail@priyaranjan.net>
2023-11-30 11:49:05 +0530

    Add supabase configuration & netlify deployment strategy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,56 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { onMount } from 'svelte';
+  import { createClient } from '@supabase/supabase-js'
+
+  // Create a single supabase client for interacting with your database
+  const supabase = createClient('https://icsokbjfkegzqmymukwh.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imljc29rYmpma2VnenFteW11a3doIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDEyNjA5NTMsImV4cCI6MjAxNjgzNjk1M30.q6yk6q6IBxN5QSxPrxPrJutmH2PcgiHtBHS6qInZTIQ')
+
+  let companies = []
+
+  onMount(async () => {
+    const { data, error } = await supabase
+      .from('companies')
+      .select()
+    
+      companies = data
+	});
+
+</script>
+
+<header>
+  <h1>Cleantech companies in India</h1>
+</header>
+<main>
+  <article>
+    <table>
+      <tbody>
+        <tr>
+          <th>Name</th>
+          <th width="300">Description</th>
+          <th>Category</th>
+          <th>Year</th>
+          <th>Website</th>
+        </tr>
+        {#each companies as { name, description, category, year, website }}
+          <tr>
+            <td>{name}</td>
+            <td>{description}</td>
+            <td>{category}</td>
+            <td>{new Date(year)?.getFullYear()}</td>
+            <td><a href={website}>{website?.split('www.')[1]?.split('/')[0]}</a></td>
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  </article>
+</main>
+
+<style>
+  table { 
+    white-space: initial;
+  }
+
+  table td, table th, table tr {
+    text-align: initial;
+  }
+</style>
